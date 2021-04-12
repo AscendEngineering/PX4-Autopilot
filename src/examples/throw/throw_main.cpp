50a6193d555c4855db9ca0283d54fde28ea53b64
Andrew <wilkinsaf@gmail.com>
2021-04-11 19:51:33 -0500

    cleanedup files a bit

--- src/examples/throw/throw_main.cpp
+++ src/examples/throw/throw_main.cpp
@@ -1,42 +1,4 @@
-/****************************************************************************
- *
- *   Copyright (C) 2015 Mark Charlebois. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name PX4 nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
 
-/**
- * @file throw_main.cpp
- * Example for Linux
- *
- * @author Mark Charlebois <charlebm@gmail.com>
- */
 
 #include "throw_example.h"
 
@@ -108,13 +70,10 @@ int PX4_MAIN(int argc, char **argv)
 	px4::init(argc, argv, "throw");
 
 	printf("throw\n");
-	printf("here1\n");
 	/* subscribe to vehicle_acceleration topic */
 	int sensor_sub_fd = orb_subscribe(ORB_ID(vehicle_acceleration));
-	printf("here2\n");
 	/* limit the update rate to 5 Hz */
 	orb_set_interval(sensor_sub_fd, 5);
-	printf("here3\n");
 
 	px4_pollfd_struct_t fds[] = {
 		{ .fd = sensor_sub_fd,   .events = POLLIN },
--- src/examples/throw/throw_start.cpp
+++ src/examples/throw/throw_start.cpp
@@ -1,42 +1,4 @@
-/****************************************************************************
- *
- *   Copyright (C) 2015 Mark Charlebois. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name PX4 nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
 
-/**
- * @file throw_start_posix.cpp
- *
- * @author Thomas Gubler <thomasgubler@gmail.com>
- * @author Mark Charlebois <mcharleb@gmail.com>
- */
 #include "throw_example.h"
 
 #include <px4_platform_common/log.h>
@@ -48,7 +10,6 @@
 
 static int daemon_task;             /* Handle of deamon task / thread */
 
-//using namespace px4;
 
 extern "C" __EXPORT int throw_main(int argc, char *argv[]);
 int throw_main(int argc, char *argv[])
@@ -61,7 +22,7 @@ int throw_main(int argc, char *argv[])
 
 	if (!strcmp(argv[1], "start")) {
 
-		if (ThrowExample::appState.isRunning()) {
+		if (ThrowApp::appState.isRunning()) {
 			PX4_INFO("already running\n");
 			/* this is not an error */
 			return 0;
@@ -78,12 +39,12 @@ int throw_main(int argc, char *argv[])
 	}
 
 	if (!strcmp(argv[1], "stop")) {
-		ThrowExample::appState.requestExit();
+		ThrowApp::appState.requestExit();
 		return 0;
 	}
 
 	if (!strcmp(argv[1], "status")) {
-		if (ThrowExample::appState.isRunning()) {
+		if (ThrowApp::appState.isRunning()) {
 			PX4_INFO("is running\n");
 
 		} else {
